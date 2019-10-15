e89a47e2fb9e60b2a43edcc6ad8eb59a29a94aa6
samSterns <sam.g.sterns@gmail.com>
2019-10-15 07:57:55 -0700

    api.js complete

---
+++ api.js
@@ -0,0 +1,122 @@
+export const productData = [
+    {
+    id: 'r2d2-suitcase',
+    name: 'R2D2 suitcase',
+    image: '../assets/bag.jpg',
+    description: 'A suitcase that can help you fly'
+    },
+    {
+    id: 'banana-slicer',
+    name: 'Banana Slicer',
+    image: '../assets/banana.jpg',
+    description: 'You will never cut your fingers again trying to slice a banana'
+    },
+    {
+    id: 'bathroom-tablet',
+    name: 'Toilet Tablet Holder',
+    image: '../assets/bathroom.jpg',
+    description: 'do not get your poop fingers on your tablet'
+    },
+    {
+    id: 'sandal-boots',
+    name: 'sandal boots',
+    image: '../assets/boots.jpg',
+    description: 'show off your toe nail polish all year'
+    },  
+    {
+    id: 'breakfast'
+    name: 'all in one breakfast appliance'
+    image: '../assets/breakfast.jpg',
+    description: 'great breakfast station for city living'
+},  
+{
+    id: 'bubblegum'
+    name: 'meatball bubblegum'
+    image: '../assets/bubblegum.jpg',
+    description: 'it is like soft jerky'
+},  
+{
+    id: 'chair'
+    name: "gumby chair"
+    image: '../assets/cahir.jpg',
+    description: 'do not think about it too much'
+},  
+{
+    id: 'cthulhu'
+    name: 'cthulhu'
+    image: '../assets/cthulhu.jpg',
+    description: 'livesize blow up yard decore perfect for any season'
+},  
+{
+    id: 'dog-duck',
+    name: 'dog-duck',
+    inamge: '../asset/dog-duck.jpg',
+    description: 'barks like a quack'
+},
+{
+    id: 'dragon',
+    name: 'dragon',
+    inamge: '../asset/dragon.jpg',
+    description: 'saving the princess by day raiding the castle at night'
+},
+{
+    id: 'pen',
+    name: 'pen',
+    inamge: '../asset/pen.jpg',
+    description: 'for those important deals'
+},
+{
+    id: 'pet-sweep',
+    name: 'pet-sweep',
+    inamge: '../asset/pet-sweep.jpg',
+    description: 'pet-sweep for the whole family'
+},
+{
+    id: 'scissors',
+    name: 'scissors',
+    inamge: '../asset/scissors.jpg',
+    description: 'do not run with these'
+},
+{
+    id: 'shark',
+    name: 'shark',
+    inamge: '../asset/shark.jpg',
+    description: 'it will eat you alive'
+},
+{
+    id: 'sweep',
+    name: 'sweep',
+    inamge: '../asset/sweep.jpg',
+    description: 'make your baby do some work'
+},
+{
+    id: 'tauntaun',
+    name: 'tauntaun',
+    inamge: '../asset/tauntaun.jpg',
+    description: 'warmest night sleep'
+},
+{
+    id: 'unicorn',
+    name: 'unicorn',
+    inamge: '../asset/unicorn.jpg',
+    description: 'no actual unicorns harmed during the making of this product'
+},
+{
+    id: 'usb',
+    name: 'usb',
+    inamge: '../asset/usb.jpg',
+    description: 'the cracken'
+},
+{
+    id: 'water-can',
+    name: 'water-can',
+    inamge: '../asset/water-can.jpg',
+    description: 'spend your money on this'
+},
+{
+    id: 'wine-glass',
+    name: 'wine-glass',
+    inamge: '../asset/wine-glass.jpg',
+    description: 'but it looks cool'
+},
+]
