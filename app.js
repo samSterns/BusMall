e1b74f6324e67a8fd64777db23d21a26f9148d61
samSterns <sam.g.sterns@gmail.com>
2019-10-18 15:26:50 -0700

    yes it finally works end of firsst part of lab

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
 import { productData } from './api.js';
 import { ProductArray } from './product-array.js';
+import { findById } from './utils/utils.js';
 
 const productImageTags = document.querySelectorAll('img');
 const productRadioTags = document.querySelectorAll('input');
 // const productName = document.getElementById('product-name');
 const product = new ProductArray(productData);
 // const radioButton = document.getElementById('radio-button');
+
+const userPicksArray = [];
+const trackedViewsArray = [];
+
+function trackUserPicks(productId) {
+    let found = findById(userPicksArray, productId);
+    if (!found) {
+        found = {
+            id: productId,
+            timesClicked: 1,
+        };
+        userPicksArray.push(found);
+    } else {
+        found.timesClicked++;
+        return;
+    }
+    const json = JSON.stringify(userPicksArray);
+    localStorage.setItem('userPicksArray', json);
+}
+
+function incrementShown(productId) {
+    let tracked = findById(trackedViewsArray, productId);
+    if (!tracked) {
+        tracked = {
+            id:productId,
+            timesShown: 1,
+        };
+        trackedViewsArray.push(tracked);
+    } else {
+        tracked.timesShown++;
+    }
+    const json = JSON.stringify(trackedViewsArray);
+    localStorage.setItem('trackedViewsArray', json);
+}
+
+
 
 const initializeNewSetOfProducts = () => {
     const randomProductOne = product.getRandomProduct();
@@ -23,42 +60,60 @@ const initializeNewSetOfProducts = () => {
     productImageTags.forEach((imageTag, i) => {
         if (i === 1) {
             imageTag.src = randomProductOne.image;
+            incrementShown(randomProductOne.id);
         } else if (i === 0) {
             imageTag.src = randomProductTwo.image; 
+            incrementShown(randomProductTwo.id);
         } else {
             imageTag.src = randomProductThree.image;
+            incrementShown(randomProductThree.id);
         }
-    });
-
-    const clickedArray = [];
-    productRadioTags.forEach((radioTag, i) => {
-
-        if (i === 1) {
-            radioTag.value = randomProductOne.id;
-        } else if (i === 0) {
-            radioTag.value = randomProductTwo.id;
-        } else {
-            radioTag.value = randomProductThree.id;
-        }
-        radioTag.addEventListener('click', (event) => {
-            let found = false; 
-            clickedArray.forEach(clickedItem => {
-                if (clickedItem.id === radioTag.value) {
-                    found = true;
-                    clickedItem.clicks += 1;
-                }
-            });
-            if (!found) {
-                clickedArray.push({
-                    id: radioTag.value, clicks: 1
-                });
-            }
-            console.log(clickedArray);
-         });
     });
 };
-
 initializeNewSetOfProducts();
+let votesRemaining = 25;
+
+productRadioTags.forEach((radioTag) => {
+    radioTag.addEventListener('click', (event) => {
+        if (event.target) {
+            trackUserPicks(event.target.value);
+            votesRemaining--;
+            if (votesRemaining === 0) {
+                document.location.href = 'reults.html';
+            }
+        }
+        initializeNewSetOfProducts();
+    });
+});
+
+// productRadioTags.forEach((radioTag, i) => {
+
+//     if (i === 1) {
+//         radioTag.value = randomProductOne.id;
+//     } else if (i === 0) {
+//         radioTag.value = randomProductTwo.id;
+//     } else {
+//         radioTag.value = randomProductThree.id;
+//     }
+//     radioTag.addEventListener('click', (event) => {
+//         let found = false; 
+//         clickedArray.forEach(clickedItem => {
+//             if (clickedItem.id === radioTag.value) {
+//                 found = true;
+//                 clickedItem.clicks += 1;
+//             }
+//         });
+//         if (!found) {
+//             clickedArray.push({
+//                 id: radioTag.value, clicks: 1
+//             });
+//         }
+//         console.log(clickedArray);
+//     });
+// });
+
+
+// initializeNewSetOfProducts();
 
 // const clickedAndShown = [];
 
